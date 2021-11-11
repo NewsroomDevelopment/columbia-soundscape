a4be51833cb29e7fd2841e35d707a05ae35b48c9
el3031 <67562724+el3031@users.noreply.github.com>
2021-11-10 22:08:31 -0500

    started some work on making globaldata.js and also the iterating function

---
+++ src/GlobalData.js
@@ -0,0 +1,17 @@
+const GlobalData = {
+    locations: [
+        {
+            name: "poop",
+            audioSource: "./audio/poop.mp3",
+            location: [100, 200],
+            description: "this is poopy"
+        },
+        {
+            name: "poop",
+            audioSource: "./audio/poop.mp3",
+            location: [100, 200],
+            description: "this is poopy"
+        }
+    ]
+}
+export default GlobalData;
---
+++ src/someIteratingFunction.js
@@ -0,0 +1,21 @@
+//this function is supposed to iterate through GlobalData.js
+//when completed, paste into App.js
+import GlobalData from './GlobalData'
+//sample code pulled from homecoming-2021
+//needs to be modified
+const Articles = this.props.data.map((el, i) => {
+    var newID = el.title;
+    var id = newID.replace(/ /g, "-");
+    console.log(id);
+    list[i] = {
+        name: el.title,
+        description: el.author
+    }
+    return <ArticleBox key={i} data={el} id={id}/>
+})
+
+
+//new version
+const Buttons = GlobalData[locations].map((el, i) => {
+    var newID = el.title;
+})
