abfcc4acfdefd31772e5897e513cfac488988a00
PatP15 <30728443+PatP15@users.noreply.github.com>
2021-11-21 16:28:08 -0500

    bla

--- src/Component/Button.js
+++ src/Component/Button.js
@@ -1,10 +1,53 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Button = ( { article } ) => {
+const ArticleWrap = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 20rem;
+    border: 3px solid #251282;
+    background: white;
+    margin: 1rem;
+`;
+
+const Link = styled.a`
+    text-decoration: none;
+    width: inherit;
+`;
+
+const ArticleImg = styled.img`
+    width: inherit;
+    max-height: 15rem;
+    overflow: hidden;
+    border-bottom: 3px solid #251282;
+`;
+
+const ArticleText = styled.div`
+    text-align: left;
+    padding: 0rem 1rem;
+    color: #251282;
+`;
+
+const Title = styled.p`
+    font-weight: bold;
+    font-size: 1.25rem;
+`;
+
+const Author = styled.p`
+`;
+
+const ArticleBox = ( { article } ) => {
     return (
-        
+        <ArticleWrap>
+            <Link href={article.article_link} target="_blank" rel="noreferrer">
+                <ArticleImg src={article.image_url} />
+                <ArticleText>
+                    <Title>{article.article_title}</Title>
+                    <Author>{article.article_authors}</Author>
+                </ArticleText>
+            </Link>
+        </ArticleWrap>
     );
 };
 
-export default Button;
+export default ArticleBox;
