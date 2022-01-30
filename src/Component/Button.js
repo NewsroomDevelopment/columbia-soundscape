f85eb635fd1700f3db687fe14a983ac81bf62f3d
PatP15 <30728443+PatP15@users.noreply.github.com>
2022-01-30 15:34:14 -0500

    undid the frying

--- src/Component/Button.js
+++ src/Component/Button.js
@@ -1,18 +1,21 @@
 
 import styled from 'styled-components';
 import './Button.css';
+import buttonImage from "../sound/download.png"
 
 
 const ButtonStyle = styled.button`
-    background-color: black;
-    color: white;
-    font-size: 20px;
-    width: 10%;
-    height: 10%;
+    background-color: rgb(137,207,240);
+    //background: url(${buttonImage});
+    color: grey;
+    font-size: calc(1em + 0.5vmin);
+    width: 3.2%;
+    height: 5%;
     position: absolute;
     top: ${(props) => props.location[0]}%;
     left: ${(props) => props.location[1]}%;
     border-radius: 5px;
+    border: 2px solid white;
     margin: 10px 0px;
     z-index:100;
     cursor: pointer;
@@ -22,21 +25,28 @@ const ButtonStyle = styled.button`
 
 
 var prevPlaying;
-
+var prevSource
 
 
 const Play = (event, audioSource) => {
     const sound = new Audio(audioSource);
-    // if (prevPlaying != null)
-    // {
-    //     prevPlaying.pause();
-    // }
-    sound.play();
-//    prevPlaying = sound;
+    if (prevPlaying != null)
+    {
+        prevPlaying.pause();
+    }
+    if(prevSource == audioSource){
+        prevPlaying.pause();
+        sound.pause();
+    }
+    else{
+        sound.play();
+        prevPlaying = sound;
+        prevSource = audioSource;
+    }
 }
 
-const Button = ({ name, location, audioSource }) => {
-    return ( <div class="tooltip"> <ButtonStyle onClick={(event) => Play(event, audioSource)} location={location}>{name}<span class="tooltiptext">Tooltip text</span></ButtonStyle>
+const Button = ({ name, location, audioSource, description }) => {
+    return ( <div class="tooltip"> <ButtonStyle onClick={(event) => Play(event, audioSource)} location={location}><span class="tooltiptext">{description}</span></ButtonStyle>
     </div>);
 }
 
