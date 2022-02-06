363c1f1439f04450727bbfbde32e6a3fe89e2bbf
PatP15 <30728443+PatP15@users.noreply.github.com>
2022-02-06 15:31:35 -0500

    hong to the rescue

--- src/Component/Button.js
+++ src/Component/Button.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import './Button.css';
 import buttonImage from "../sound/button.png"
+import React, { useState } from 'react';
 
 
 const ButtonStyle = styled.button`
@@ -8,6 +9,7 @@ const ButtonStyle = styled.button`
     background: url(${buttonImage});
     background-size: cover;
     font-size: calc(1em + 0.5vmin);
+    opacity: ${(props) => props.opacity};
     width: 3%;
     height: 5%;
     position: absolute;
@@ -16,6 +18,10 @@ const ButtonStyle = styled.button`
     border: none;
     cursor: pointer;
     Access-Control-Allow-Origin: *
+    .click:hover {
+        transform: scale(0.6);
+        transition-duration: 0.5s;
+    }
 `;
 
 
@@ -25,26 +31,31 @@ var prevPlaying;
 var prevSource
 
 
-const Play = (event, audioSource) => {
+const Play = (event, audioSource, setOpacity) => {
     const sound = new Audio(audioSource);
-    if (prevPlaying != null)
-    {
+    if (prevPlaying != null) {
         prevPlaying.pause();
+        setOpacity(1);
     }
-    if(prevSource == audioSource){
+    if (prevSource == audioSource) {
         prevPlaying.pause();
         sound.pause();
         prevSource = null;
+        setOpacity(1);
     }
-    else{
+    else {
         sound.play();
         prevPlaying = sound;
         prevSource = audioSource;
+        setOpacity(0.7);
     }
 }
 
 const Button = ({ name, location, audioSource, description }) => {
-    return ( <div class="tooltip"> <ButtonStyle onClick={(event) => Play(event, audioSource)} location={location}><span class="tooltiptext">{description}</span></ButtonStyle>
+    const [opacity, setOpacity] = useState(1);
+    return (<div class="tooltip">
+        <ButtonStyle opacity={opacity} onClick={(event) => Play(event, audioSource, setOpacity)} location={location}><span class="tooltiptext">{description}</span></ButtonStyle>
+        
     </div>);
 }
 
